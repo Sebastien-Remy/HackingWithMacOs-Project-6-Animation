842815a175ed82f51f0203266519e341bcc88241
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-23 10:10:54 +0200

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Sebastien REMY on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
