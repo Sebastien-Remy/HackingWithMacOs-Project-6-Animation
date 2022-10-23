83cad5602cfc8e0c468595d3c91cce6944c9d77a
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-23 10:43:52 +0200

    Binding

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -12,38 +12,23 @@ struct ContentView: View {
     @State private var animationAount = 1.0
     
     var body: some View {
-        Text("Click me")
-            .onTapGesture {
-                //animationAount += 1
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .overlay(
-            Circle()
-                .stroke(.red)
+        print (animationAount)
+        return VStack {
+         
+            Stepper("Scale amount", value: $animationAount.animation(.easeInOut(duration: 1)), in: 1...10)
+            
+            Text("Click me")
+                .onTapGesture {
+                    animationAount += 1
+                }
+                .padding(40)
+                .background(.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .padding(100)
                 .scaleEffect(animationAount)
-                .opacity(2 - animationAount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAount
-                    )
-            )
-            .padding(100)
-            .onAppear {
-                animationAount = 2
-            }
-//            .blur(radius: 0.5)
-//            .animation(.default, value: animationAount)
-//            .animation(.easeOut, value: animationAount)
-//            .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAount)
- //           .animation(.easeInOut(duration: 2)
- //               .delay(1),
- //                      value: animationAount)
-  //          .animation(.easeInOut(duration: 1)
- //               .repeatForever(autoreverses: true), value: animationAount)
+            
+        }
     }
     
 }
